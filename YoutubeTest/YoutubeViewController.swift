e38cd1e4a128cf74cdaf17c6c7aeae544a7ec98f
gobright <gobright12@gmail.com>
2018-07-02 15:41:40 +0900

    Initial Commit

---
+++ YoutubeTest/YoutubeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeTest
+//
+//  Created by DONGHYUN KIM on 2018. 7. 2..
+//  Copyright © 2018년 dkim. All rights reserved.
+//
+
+import UIKit
+import youtube_ios_player_helper
+
+class YoutubeViewController: UIViewController {
+
+    @IBOutlet weak var playerView: YTPlayerView! {
+        didSet {
+            playerView.load(withVideoId: "KwpnECo7jhs")
+        }
+    }
+}
+
